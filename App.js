c408e4b51dee7000e07b140180e48e05c9cc48af
Collin McKinney <collinmmckinney@gmail.com>
2018-03-31 21:57:44 -0400

    Trying to auth, server error

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
@@ -11,10 +11,12 @@ import { Provider } from 'react-redux';
 import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
 import ReduxLink from 'apollo-link-redux';
 import { onError } from 'apollo-link-error';
+import { setContext } from 'apollo-link-context';
+import gql from 'graphql-tag';
 
 import { HomeContainer } from './src/screens';
 
-const URL = 'localhost:8080';
+const GRAPHQL_URL = 'https://api.graph.cool/simple/v1/cjfg0gyqm0kw60103ql11s2vc';
 
 // Redux
 const store = createStore(
@@ -25,20 +27,62 @@ const store = createStore(
 
 // Apollo
 const cache = new ReduxCache({ store });
+
 const reduxLink = new ReduxLink(store);
 const errorLink = onError((errors) => {
     console.log(errors);
 });
-const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
+const httpLink = createHttpLink({ uri: GRAPHQL_URL });
+const authLink = setContext(async (_, { headers }) => {
+    // Set token if it exists in local storage:
+    const token = await AsyncStorage.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        }
+    };
+});
 const link = ApolloLink.from([
     reduxLink,
     errorLink,
     httpLink,
+    authLink
 ]);
 const client = new ApolloClient({
     link,
-    cache,
+    cache
 });
+
+client.mutate({
+    mutation: gql`
+        mutation {
+            signupUser(email: "testing@test.com", username: "testing", password: "testing") {
+                id
+                token
+            }
+        }
+    `
+})
+    .then(({ data }) => {
+        console.log(data);
+        AsyncStorage.setItem('token', data.signupUser.token).then(() => {
+            client.query({
+                query: gql`
+                    query {
+                        loggedInUser() {
+                            id
+                        }
+                    }
+                `
+            })
+                .then((result) => {
+                    console.log(result);
+                })
+                .catch(error => console.error(error));
+        });
+    })
+    .catch(error => console.error(error));
 
 const AppNavigator = StackNavigator({
     HomeScreen: {
