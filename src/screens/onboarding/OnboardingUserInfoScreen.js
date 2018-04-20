3b7a46efb773c1c7df43454e3623368d9427cc5a
lolomitch <lakami96@gmail.com>
2018-04-20 15:10:20 -0400

    yay

--- src/screens/onboarding/OnboardingUserInfoScreen.js
+++ src/screens/onboarding/OnboardingUserInfoScreen.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, Picker } from 'react-native';
 import PropTypes from 'prop-types';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 import { colors } from '../../style';
-import { Button, Form } from '../../components';
+import { Button } from '../../components';
 
 const FIELDS = [
     { key: 'firstName', label: "What's your first name?" },
@@ -19,20 +19,42 @@ const styles = StyleSheet.create({
         paddingTop: 80,
         backgroundColor: 'white'
     },
-    form: {
+    progressBar: {
         flex: 1,
-        paddingHorizontal: 18,
-        alignItems: 'stretch'
     },
     screen: {
-        flex: 1,
+        flex: 4,
         borderColor: colors.primary,
         borderWidth: 3,
         borderRadius: 8
     },
     nextButton: {
         marginBottom: 40,
-        marginTop: 30
+        marginTop: 30,
+        backgroundColor: colors.primaryDarker
+    },
+    nextWrapper: {
+        flex: 2,
+        marginLeft: 5
+    },
+    backButton: {
+        marginBottom: 40,
+        marginTop: 30,
+        backgroundColor: colors.primaryDarker,
+        opacity: 0.5
+    },
+    backWrapper: {
+        flex: 1,
+        marginRight: 5
+    },
+    buttons: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    entry: {
+        flex: 1,
+        flexDirection: 'row'
     }
 });
 
@@ -67,21 +89,49 @@ export default class OnboardingUserInfoScreen extends Component {
 
     render() {
         const fields = FIELDS.map(field => ({ ...field, value: this.state[field.key] }));
-
         return (
             <View style={styles.container}>
+                <View style={styles.progressBar} />
                 <KeyboardAwareScrollView style={styles.screen}>
-                    <Form
-                        fields={fields}
-                        onChange={this.handleFormChange}
-                        style={styles.form}
-                    />
+                    <View style={styles.entry}>
+                        <Text>What{"'"}s your first name?</Text>
+                    </View>
+                    <View style={styles.entry}>
+                        <Text>Last name?</Text>
+                    </View>
+                    <View style={styles.entry}>
+                        <Text>How old are you?</Text>
+                        <Picker
+                            selectedValue={fields.age}
+                            style={{ height: 50, width: 100 }}
+                            onValueChange={(itemValue, itemIndex) => this.setState({ [fields.age]: itemValue })}
+                        >
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                        </Picker>
+                    </View>
+                    <View style={styles.entry}>
+                        <Text>What are your pronouns?</Text>
+                    </View>
+                    <View style={styles.entry}>
+                        <Text>Add a picture</Text>
+                    </View>
                 </KeyboardAwareScrollView>
-                <Button
-                    onPress={this.handlePressNext}
-                    label="NEXT"
-                    style={styles.nextButton}
-                />
+                <View style={styles.buttons}>
+                    <View style={styles.backWrapper}>
+                        <Button
+                            label="←"
+                            style={styles.backButton}
+                        />
+                    </View>
+                    <View style={styles.nextWrapper}>
+                        <Button
+                            onPress={this.handlePressNext}
+                            label="NEXT"
+                            style={styles.nextButton}
+                        />
+                    </View>
+                </View>
             </View>
         );
     }
